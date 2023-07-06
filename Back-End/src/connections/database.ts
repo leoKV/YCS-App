d09f8d05231807b40cafdd90f2afe186f8c471c3
ManuelAvil <88950498+ManuelAvil@users.noreply.github.com>
2023-07-06 00:18:56 -0600

    Update database.ts

--- Back-End/src/connections/database.ts
+++ Back-End/src/connections/database.ts
@@ -1,6 +1,6 @@
 //const sql = require('mssql')
 import mysql from 'promise-mysql'
-import chain from '../config/connection';
- 
+import chain from '../models/connection';
+
 const pool= mysql.createPool(chain.database);
 export default pool;
