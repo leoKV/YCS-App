13a867579bbfa56ef0e0eb8c3e5782129b5db7f0
José Manuel Matehuala Avilés <josemanuel.ma.av@gmail.com>
2023-07-05 23:49:41 -0600

    Cambios en connection.ts

--- Back-End/src/config/connection.ts
+++ Back-End/src/config/connection.ts
@@ -3,7 +3,7 @@ export default{
         host:'127.0.0.1',
         port:3306,
         user:'admin',
-        password:'admin',
-        database:'bd_ycs'
+        password:'admin123',
+        database:'BD_YCS'
     }
 }
