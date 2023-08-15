2cb93ce798fce3ab25aab120532248f1aebe1716
José Manuel Matehuala Avilés <josemanuel.ma.av@gmail.com>
2023-08-15 17:32:40 -0600

    Cambios compilado

--- Back-End/src/validators/UsuarioValidatorRules.ts
+++ Back-End/src/validators/UsuarioValidatorRules.ts
@@ -2,19 +2,19 @@ import {body} from "express-validator";
 
 export const insertValidationRules = () => {
     return[
-        body("nombre").trim().not().isEmpty().withMessage("Campo requerido")
-          .isLength({min:3,max:50}).withMessage("Rango Incorrecto"),
-        // body("apellidoPaterno").trim().not().isEmpty().withMessage("Campo requerido")
-        // .isLength({min:3,max:50}).withMessage("Rango Incorrecto"),
-        // body("apellidoMaterno").trim().not().isEmpty().withMessage("Campo requerido")
-        // .isLength({min:3,max:50}).withMessage("Rango Incorrecto"),
-        // body("contrasenia").trim().not().isEmpty().custom(value => !/\s/.test(value)).withMessage("Campo requerido")
-        // .isLength({min:8,max:100}).withMessage("Rango Incorrecto"),
-        // body("email").trim().not().isEmpty().withMessage("Campo requerido")
-        // .isLength({min:10,max:50}).withMessage("Rango Incorrecto")
-        // .isEmail().withMessage("Formato Incorrecto"),
-        // body("telefono").trim().not().isEmpty().withMessage("Campo requerido"),
-        // body("roles").isArray({min:1}).withMessage("Formato Incorrecto")
+        // body("nombre").trim().not().isEmpty().withMessage("Campo requerido")
+        //   .isLength({min:3,max:50}).withMessage("Rango Incorrecto"),
+        body("apellidoPaterno").trim().not().isEmpty().withMessage("Campo requerido")
+        .isLength({min:3,max:50}).withMessage("Rango Incorrecto"),
+        body("apellidoMaterno").trim().not().isEmpty().withMessage("Campo requerido")
+        .isLength({min:3,max:50}).withMessage("Rango Incorrecto"),
+        body("contrasenia").trim().not().isEmpty().custom(value => !/\s/.test(value)).withMessage("Campo requerido")
+        .isLength({min:8,max:100}).withMessage("Rango Incorrecto"),
+        body("email").trim().not().isEmpty().withMessage("Campo requerido")
+        .isLength({min:10,max:50}).withMessage("Rango Incorrecto")
+        .isEmail().withMessage("Formato Incorrecto"),
+        body("telefono").trim().not().isEmpty().withMessage("Campo requerido"),
+        body("roles").isArray({min:1}).withMessage("Formato Incorrecto")
     ]
 }
 
