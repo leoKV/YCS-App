cbc4119707b78db422a434bf13ae4b3e4f15fba6
José Manuel Matehuala Avilés <josemanuel.ma.av@gmail.com>
2023-08-15 17:58:31 -0600

    Cambios compilado

--- Back-End/build/validators/usuarioValidatorRules.js
+++ Back-End/build/validators/usuarioValidatorRules.js
@@ -8,8 +8,8 @@ const insertValidationRules = () => {
             .isLength({ min: 3, max: 50 }).withMessage("Rango Incorrecto"),
         (0, express_validator_1.body)("apellidoPaterno").trim().not().isEmpty().withMessage("Campo requerido")
             .isLength({ min: 3, max: 50 }).withMessage("Rango Incorrecto"),
-        //     body("apellidoMaterno").trim().not().isEmpty().withMessage("Campo requerido")
-        //     .isLength({min:3,max:50}).withMessage("Rango Incorrecto"),
+        (0, express_validator_1.body)("apellidoMaterno").trim().not().isEmpty().withMessage("Campo requerido")
+            .isLength({ min: 3, max: 50 }).withMessage("Rango Incorrecto"),
         //     body("contrasenia").trim().not().isEmpty().custom(value => !/\s/.test(value)).withMessage("Campo requerido")
         //     .isLength({min:8,max:100}).withMessage("Rango Incorrecto"),
         //     body("email").trim().not().isEmpty().withMessage("Campo requerido")
