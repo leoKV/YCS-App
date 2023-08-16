b4b7583f71450e593b713db08eb3522fcc0f0078
José Manuel Matehuala Avilés <josemanuel.ma.av@gmail.com>
2023-08-16 15:32:35 -0600

    backend

--- Back-End/src/config/connection.ts
+++ Back-End/src/config/connection.ts
@@ -1,9 +1,9 @@
-export default{
-    database:{
-        host:'db-mysql-sfo3-98336-do-user-14490683-0.b.db.ondigitalocean.com',
-        port:25060,
-        user:'doadmin',
-        password:'AVNS_o3OnNnqS4r5BLhFoCkx',
-        database:'BD_YCS'
+export default {
+    database: {
+        host: 'db-mysql-sfo3-98336-do-user-14490683-0.b.db.ondigitalocean.com',
+        port: 25060,
+        user: 'doadmin',
+        password: 'AVNS_o3OnNnqS4r5BLhFoCkx',
+        database: 'BD_YCS'
     }
 }
