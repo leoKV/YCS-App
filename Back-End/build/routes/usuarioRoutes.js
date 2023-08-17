2fd20055f2a4b5cd10178b92a251ffbd8bf31719
José Manuel Matehuala Avilés <josemanuel.ma.av@gmail.com>
2023-08-17 12:14:32 -0600

    cambio categoriaRoutes.ts

--- Back-End/build/routes/usuarioRoutes.js
+++ Back-End/build/routes/usuarioRoutes.js
@@ -2,7 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const usuarioController_1 = require("../controllers/usuarioController");
-const UsuarioValidatorRules_1 = require("../validators/UsuarioValidatorRules");
+// import { insertUserValidationRules } from "../validators/UsuarioValidatorRules";
+const testValidationRule_1 = require("../validators/testValidationRule");
 const validatorCheck_1 = require("../middlewares/validatorCheck");
 const jwtCheck_1 = require("../middlewares/jwtCheck");
 class UsuarioRoutes {
@@ -14,9 +15,9 @@ class UsuarioRoutes {
         //Listar
         this.router.get("/", [jwtCheck_1.jwtCheck], usuarioController_1.usuarioController.listar);
         //Insertar
-        this.router.post("/", (0, UsuarioValidatorRules_1.insertUserValidationRules)(), [jwtCheck_1.jwtCheck, validatorCheck_1.validate], usuarioController_1.usuarioController.insertar);
+        this.router.post("/", (0, testValidationRule_1.insertUserValidator)(), [jwtCheck_1.jwtCheck, validatorCheck_1.validate], usuarioController_1.usuarioController.insertar);
         //Actualizar
-        this.router.put("/", [jwtCheck_1.jwtCheck, validatorCheck_1.validate], usuarioController_1.usuarioController.actualizar);
+        this.router.put("/", [jwtCheck_1.jwtCheck], usuarioController_1.usuarioController.actualizar);
         //Eliminar
         this.router.delete("/:idUsuario", usuarioController_1.usuarioController.eliminar);
     }
